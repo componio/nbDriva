2e38a37ceee2fd9626c36b13530409e841ac32ef
tpinky <thomas.pinkowski@componio.net>
2015-01-05 14:40:45 +0100

    Revert "Action + Dialog added for Module Properties"
    This reverts commit d40ad2a0ef64e898f15f606932333fe6bf812e2a.

--- net.componio.opencms.projectstructure.plugin/src/net/componio/opencms/projectstructure/plugin/dialogs/ImportModulePluginPanel.java
+++ net.componio.opencms.projectstructure.plugin/src/net/componio/opencms/projectstructure/plugin/dialogs/ImportModulePluginPanel.java
@@ -16,8 +16,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import org.apache.tools.ant.module.api.support.ActionUtils;
 import org.openide.filesystems.FileObject;
@@ -32,7 +30,7 @@ public class ImportModulePluginPanel extends javax.swing.JPanel {
     final JFileChooser fileChooser = new JFileChooser();
     final List<File> allSelectedFiles = new ArrayList<File>();
     final FileObject buildScript;
-    private final File nbDrivaConfigFile;
+    private File nbDrivaConfigFile;
 
     /**
      * Creates new form ImportModulePluginPanel
@@ -150,9 +148,6 @@ public class ImportModulePluginPanel extends javax.swing.JPanel {
         try {
             updateNbDrivaConfigFile();
             ActionUtils.runTarget(buildScript, new String[]{"build_import_module"}, null);
-            JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(this);
-            frame.setVisible(false);
-            frame.setEnabled(false);
         } catch (IOException ex) {
             Exceptions.printStackTrace(ex);
         } catch (IllegalArgumentException ex) {
